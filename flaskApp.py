8d304654eb3fb0b2689c28cfac5ceff73393a157
Oliver <obaker@users.noreply.github.com>
2023-03-28 23:29:15 +0100

    Added form and JSON post to add alarms, set alarms displayed in table on /

--- flaskApp.py
+++ flaskApp.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 import sqlite3
 import json
-from time import localtime, strftime
+from time import localtime, strftime, sleep
 from sys import exit, argv
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 from flask_cors import CORS
 
 dbFile = "clock.db"
 
+weekdayNumbers = [128, 64, 32, 16, 8, 4, 2]
+weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday",
+            "Friday", "Saturday", "Sunday"]
+
 
 class Alarm:
     """A class for alarms"""
+
     def __init__(self, alarmID, time, activeDays, repeat, radioStation):
         self.alarmID = alarmID
         self.time = time
@@ -33,7 +38,7 @@ def readDB():  # Reads the DB storing alarms in a list of Alarm
     weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday",
                 "Friday", "Saturday", "Sunday"]
     db = sqlite3.connect(dbFile)
-    c = db.cursor()
+    # c = db.cursor()
     alarmList = []
     for dow, time, alarmID, radio in db.execute(
      "SELECT dow, time, alarm_ID, radio FROM alarms ORDER BY time ASC;"):
@@ -54,33 +59,82 @@ def readDB():  # Reads the DB storing alarms in a list of Alarm
     return alarmList
 
 
+def addAlarm(time, dow, radio):
+    db = sqlite3.connect(dbFile)
+    c = db.cursor()
+    c.execute("INSERT INTO alarms VALUES (NULL, '" + time + "' , "+str(dow)+", "+str(radio)+")")
+    db.commit()
+    db.close()
+    print("Alarm added to DB.")
+
+
+def removeAlarm(alarmID):
+    db = sqlite3.connect(dbFile)
+    c = db.cursor()
+    c.execute("DELETE FROM alarms WHERE alarm_ID = " + alarmID)
+
+
+def refreshAlarmList(alarms):
+    alarms.clear()
+    for alarm in readDB():
+        alarms.append(alarm.toJson())
+    return None
+
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 alarms = []
-for alarm in readDB():
-    alarms.append(alarm.toJson())
-
-
-@app.route('/alarms', methods=['GET', 'POST'])
-def all_books():
-    response_object = {'status': 'success'}
-    #    if request.method == 'POST':
-    #    post_data = request.get_json()
-    #    time =   post_data.get('time'),
-    #    dow =    post_data.get('dow'),
-    #    radio =    post_data.get('radio'),
-    #    response_object['message'] = 'alarm added!'
-    #    print (time[0])
-    #    print (dow[0])
-    #    print (radio[0])
-    #    add_alarm(time[0],dow[0],radio[0])
-    return alarms
+refreshAlarmList(alarms)
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/alarms', methods=['GET', 'POST'])
+def alarmsRest():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        time = post_data.get('time'),
+        dow = post_data.get('dow'),
+        radio = post_data.get('radio')
+        try:
+            addAlarm(time[0], dow[0], radio[0])
+            response_object['message'] = 'alarm added!\n'
+            refreshAlarmList(alarms)
+        except:
+            response_object['message'] = 'DB write fail\n'
+        return response_object['message']
+    if request.method == 'GET':
+        return alarms
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def addForm():
+    if request.method == 'GET':
+        return render_template('addform.html')
+    if request.method == 'POST':
+        time = request.form['alarmTime']
+        activeDays = (request.form.getlist('days'))
+        dow = sum(list(map(int, activeDays)))
+        try:
+            addAlarm(time, dow, 0)
+            refreshAlarmList(alarms)
+        except:
+            print("db error")
+        return redirect(url_for('home'))
+
+
+@app.route('/delete/<alarmID>', methods=['PUT', 'DELETE'])
+def deleteAlarm(alarmID):
+    response_object = {'status': 'success'}
+    if request.method == 'DELETE':
+        removeAlarm(alarmID)
+        response_object['message'] = 'Alarm deleted'
+    return jsonify(response_object)
 
 
 if __name__ == '__main__':
